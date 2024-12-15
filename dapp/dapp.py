3589887f3afe637eb8a720b9b4e7611df7840fb1
gabriel <ext.gabriel.brandao@aurenenergia.com.br>
2024-12-15 15:39:07 -0300

    adicinando payload dos onibus no inspect

--- dapp/dapp.py
+++ dapp/dapp.py
@@ -8,17 +8,46 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
+def hex2str(hex):
+    """
+    Decodes a hex string into a regular string
+    """
+    return bytes.fromhex(hex[2:]).decode("utf-8")
+
+
+def str2hex(str):
+    """
+    Encodes a string as a hex string
+    """
+    return "0x" + str.encode("utf-8").hex()
+
+
+bus_data = []
 
 def handle_advance(data):
     logger.info(f"Received advance request data {data}")
 
-    payload = data["payload"]
+    payload = hex2str(data["payload"])
+
+    logger.info(f"Dados a serem gravados: {payload}")
+
+    bus_data.append(payload)
 
     return "accept"
 
 
 def handle_inspect(data):
     logger.info(f"Received Brandão e Marcelo inspect request data {data}")
+
+    report = {
+        "payload": str2hex(
+            f"/nDados dos Ônibus: \n{bus_data}"
+        )
+    }
+
+    response = requests.post(rollup_server + "/report", json=report)
+    logger.info(f"Received report status {response.status_code}")
+
     return "accept"
 
 
