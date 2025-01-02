89b5e4bf90564e842a8b852b4fd58f56b3e3b0df
gabriel <ext.gabriel.brandao@aurenenergia.com.br>
2025-01-02 13:28:44 -0300

    add dashboard styling and chart components for data visualization

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,7 +1,12 @@
+import { CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from 'chart.js';
 import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import { fetchDashboardData } from '../api/dashboardService';
 import { DashboardData, Payload } from '../types/DashboardData';
 import { hex2str } from '../utils/ether';
+import './styles.css';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const Dashboard: React.FC = () => {
   const [dados, setDados] = useState<Payload['dados'] | null>(null);
@@ -32,10 +37,65 @@ const Dashboard: React.FC = () => {
     return <div>Loading...</div>;
   }
 
+  const chartData = {
+    labels: dados?.map((dado) => dado.linha) || [],
+    datasets: [
+      {
+        label: 'Frota Programada',
+        data: dados?.map((dado) => dado.frotaProgramada) || [],
+        backgroundColor: 'rgba(0, 0, 255, 0.2)',
+        borderColor: 'rgba(0, 0, 255, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Frota Disponível',
+        data: dados?.map((dado) => dado.frotaDisponivel) || [],
+        backgroundColor: 'rgba(255, 0, 0, 0.2)',
+        borderColor: 'rgba(255, 0, 0, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    plugins: {
+      legend: {
+        labels: {
+          color: 'black',
+        },
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: 'black',
+        },
+      },
+      y: {
+        ticks: {
+          color: 'black',
+        },
+      },
+    },
+  };
+
   return (
-    <div>
+    <div className='dashboard-title'>
       <h1>Dashboard Dados</h1>
-      <pre>{JSON.stringify(dados, null, 2)}</pre>
+      <div className='charts-grid'>
+        <div className='chart-container'>
+          <Line data={chartData} options={chartOptions} width={500} height={400} />
+        </div>
+        <div className='chart-container'>
+          <Line data={chartData} options={chartOptions} width={500} height={400} />
+        </div>
+        <div className='chart-container'>
+          <Line data={chartData} options={chartOptions} width={500} height={400} />
+        </div>
+        <div className='chart-container'>
+          <Line data={chartData} options={chartOptions} width={500} height={400} />
+        </div>
+      </div>
     </div>
   );
 };
